476090a1c9cabb87023ca3dde04f266192ecda25
Abhi85shek <abhishek.kumar@arkilos.com>
2025-07-07 22:21:44 +0530

    Changes in the ENV File

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 // export const DOMAIN_URL = 'https://p0ydrzp827.execute-api.ca-central-1.amazonaws.com/prod';
 
-export const DOMAIN_URL = "http://10.0.2.2:8000"
+// export const DOMAIN_URL = "http://10.0.2.2:8000"
 // export const DOMAIN_URL = 'https://laceroims.com'
 
-// export const DOMAIN_URL = "https://163px9bfti.execute-api.us-east-1.amazonaws.com/dev"
+export const DOMAIN_URL =
+  'https://163px9bfti.execute-api.us-east-1.amazonaws.com/dev';
